f4bc490aaa74d49ec490d0a4a1d50d4c702ce797
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2013-04-25 15:16:39 +0200

    Better error messaging

--- template.js
+++ template.js
@@ -1,30 +1,32 @@
 var Template = (function() {
+  var firstSelector = 'template:first, script[type="text/html-template"]:first, [data-type=template]:first';
   var errorMessage = function(id) {
-    return '[template.js] An error occured, template "' + id + '" does not exist';
+    return '[template.js] An error occured, template "' + id + '" does not exist<br/>';
   };
-  var errorJquery = function() {
-    return '[template.js] An error occured, possible wrong jQuery selector.';
+  var errorJquery = function(selector) {
+    return '[template.js] An error occured, possible wrong jQuery selector ( ' + selector + ' )<br/>';
   };
   var Template = function(id) {
     var htmlTemplate = errorMessage(id);
     if (typeof id === 'undefined') {
       var asTemplate = [];
       try {
-        asTemplate = $('template:first, script[type="text/html-template"]:first, [data-type=template]:first');
-      } catch(e) { htmlTemplate = errorJquery(); console.error(e); }
+        asTemplate = $(firstSelector);
+      } catch(e) { htmlTemplate += errorJquery(firstSelector); console.error(e); }
       if (asTemplate.length > 0) {
         htmlTemplate = asTemplate.first().html();
       } 
     } else {
       var jquerySelector = [];
-      try { jquerySelector = $(id); } catch(e) { htmlTemplate = errorJquery(); console.error(e); }
+      try { jquerySelector = $(id); } catch(e) { htmlTemplate += errorJquery(id); console.error(e); }
       if (jquerySelector.length > 0) {
         htmlTemplate = jquerySelector.first().html();
       } else {
         var asTemplate = [];
+        var selector = Mustache.render('template#{{id}}, script[type="text/html-template"]#{{id}}, [data-type=template]#{{id}}', {id: id});
         try {
-          asTemplate = $('template#' + id + ', script[type="text/html-template"]#' + id + ', [data-type=template]#' + id);
-        } catch(e) { htmlTemplate = errorJquery(); console.error(e); }
+          asTemplate = $(selector);
+        } catch(e) { htmlTemplate += errorJquery(selector); console.error(e); }
         if (asTemplate.length > 0) {
           htmlTemplate = asTemplate.first().html();
         }
